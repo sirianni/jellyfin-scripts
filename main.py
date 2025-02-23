ca665cbc2b416cca8054618086694cd171361565
Eric Sirianni <eric.sirianni@gmail.com>
2025-02-23 12:36:55 -0500

    Deinterlace

--- main.py
+++ main.py
@@ -72,7 +72,8 @@ class Recording:
                     "-i", input_file,
                     "-i", self.chapter_ffmeta,
                     "-map_metadata", "1",
-                    "-c:v", "libx264", "-preset", "medium", "-crf", "26",
+                    "-filter:v", "yadif", # deinterlace
+                    "-c:v", "libx264", "-preset", "medium", "-crf", "25",
                     "-acodec", "aac", "-ar", "44100", "-b:a", "256k",
                     "-y",  # Overwrite output files without asking
                     "-stats_period", "15",
