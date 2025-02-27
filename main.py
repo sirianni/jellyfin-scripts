40e1926057384d1afbe0be5ed1ee2f569038b5dc
Eric Sirianni <eric.sirianni@gmail.com>
2025-02-26 22:31:42 -0500

    Missing comma!

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ class Recording:
                     "-map_metadata", "1",
                     "-filter:v", "yadif", # deinterlace
                     "-codec:v", "libx264", "-preset", "medium", "-crf", "24",
-                    "-codec:a", "copy"
+                    "-codec:a", "copy",
                     "-y",  # Overwrite output files without asking
                     "-stats_period", "15",
                     self.transcoded_file,
